880b9af3637fd7c5c5c9edee4d351f13f352ae6d
Zumatra Hia <61663462+unholy182000@users.noreply.github.com>
2021-03-21 22:42:01 +0300

    Add files via upload

---
+++ NumberGame.kt
@@ -0,0 +1,27 @@
+package com.zumatrahia.deneme
+
+import java.lang.Exception
+
+var trueAnswer = (1..3).random()
+var answer : Any? = 0
+
+fun main(){
+    try { while (true) {
+            println("Guess Number???")
+            answer = readLine()
+            if (answer == "qqq") break
+            answer = answer.toString().toInt()
+            if (answer != trueAnswer) continue
+            else {
+                println("You Guessed Correctly!!!")
+                trueAnswer = (1..3).random()
+                continue
+            }
+        }
+    }
+    catch (e: Exception){
+        println("!!! Enter Number !!!")
+        main()
+    }
+    println("You exited Game!!!")
+}
